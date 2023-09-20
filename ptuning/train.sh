f3913af5e4c057981ba509bed4eba300392ea188
sunxun <sunxun@ltidc.cn>
2023-09-20 16:02:47 +0800

    测试

--- ptuning/train.sh
+++ ptuning/train.sh
@@ -13,7 +13,7 @@ torchrun --standalone --nnodes=1 --nproc-per-node=$NUM_GPUS main.py \
     --model_name_or_path /mnt/workspace/THUDM/chatglm2-6b \
     --output_dir output/adgen-chatglm2-6b-pt-$PRE_SEQ_LEN-$LR \
     --overwrite_output_dir \
-    --max_source_length 64 \
+    --max_source_length 128 \
     --max_target_length 128 \
     --per_device_train_batch_size 1 \
     --per_device_eval_batch_size 1 \
