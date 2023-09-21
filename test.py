2ba7cc9762edd1c6a9a8455fbbbd9beaac15d30d
sunxun <sunxun@ltidc.cn>
2023-09-21 15:30:58 +0800

    OK

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from transformers import AutoModel, AutoTokenizer
 
 tokenizer = AutoTokenizer.from_pretrained("/mnt/workspace/THUDM/chatglm2-6b", trust_remote_code=True)
-model = AutoModel.from_pretrained("/mnt/workspace/THUDM\chatglm2-6b", trust_remote_code=True).cuda()
+model = AutoModel.from_pretrained("/mnt/workspace/THUDM/chatglm2-6b", trust_remote_code=True).cuda()
 model = model.eval()
 response,history = model.chat(tokenizer, "你好", history=[])
 print(response)
