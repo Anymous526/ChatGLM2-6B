93147f0f25497f9bcb450a3308b44e2ae1063f68
sunxun <sunxun@ltidc.cn>
2023-09-27 09:36:50 +0800

    Ok

--- before.py
+++ before.py
@@ -1,6 +1,5 @@
 from transformers import AutoModel, AutoTokenizer
 
-
 model_path = "/mnt/workspace/THUDM/chatglm-6b"
 # 载入Tokenizer
 tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
@@ -10,11 +9,11 @@ model = model.eval()
 
 
 def display_answer(model, query, history=[]):
-    for response, history in model.stream_chat(tokenizer, query, history=history):
+  for response, history in model.stream_chat(tokenizer, query, history=history):
     return history
 
 
 if __name__ == '__main__':
-    query = input("\n用户:")  # "类型#上衣\*材质#牛仔布\*颜色#白色\*风格#简约\*图案#刺绣\*衣样式#外套\*衣款式#破洞"
-    answer = display_answer(model, query)
-    print(answer)
+  query = input("\n用户:")  # "类型#上衣\*材质#牛仔布\*颜色#白色\*风格#简约\*图案#刺绣\*衣样式#外套\*衣款式#破洞"
+  answer = display_answer(model, query)
+  print(answer)
